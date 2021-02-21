376f6eab9f2376843e5b65401fcea783bb4f7ade
Pavol Hradsky <hradskypalo@gmail.com>
2021-02-21 18:29:54 +0100

    first branch

---
+++ main.c
@@ -0,0 +1,33 @@
+/**
+ * DSA Zadianie 1 - Spravca pamati
+ * 
+ * FIIT STU 2021
+ * Pavol Hradsky
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+void *memory_alloc(unsigned int size){
+    
+}
+
+int memory_free(void *valid_ptr){
+
+}
+
+int memory_check(void *ptr){
+
+}
+
+void memory_init(void *ptr, unsigned int size){
+
+}
+
+int main(void){
+
+    char region[50];
+    memory_init(region, 50);
+
+    return 0;
+}
