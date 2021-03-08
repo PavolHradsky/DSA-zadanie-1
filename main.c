efa8bd803d43bda6745274a22a7ae4745418f3ad
Pavol Hradsky <hradskypalo@gmail.com>
2021-03-08 20:12:05 +0100

    final version 01

--- main.c
+++ main.c
@@ -303,77 +303,6 @@ void memory_init(void *ptr, unsigned int size){
 
 }
 
-void z1_testovac(char *region, char **pointer, int minBlock, int maxBlock, int minMemory, int maxMemory, int testFragDefrag) {
-    unsigned int allocated = 0;
-    unsigned int mallocated = 0;
-    unsigned int allocated_count = 0;
-    unsigned int mallocated_count = 0;
-    unsigned int i = 0;
-    int random_memory = 0;
-    int random = 0;
-    memset(region, 0, 100000);
-    random_memory = (rand() % (maxMemory-minMemory+1)) + minMemory;
-    memory_init(region + 500, random_memory);
-    if (testFragDefrag) {
-        do {
-            pointer[i] = memory_alloc(8);
-            if (pointer[i])
-                i++;
-        } while (pointer[i]);
-        for (int j = 0; j < i; j++) {
-            if (memory_check(pointer[j])) {
-                memory_free(pointer[j]);
-            }
-            else {
-                printf("Error: Wrong memory check.\n");
-            }
-        }
-    }
-    i = 0;
-    while (allocated <= random_memory-minBlock) {
-        random = (rand() % (maxBlock-minBlock+1)) + minBlock;
-        if (allocated + random > random_memory)
-            continue;
-        allocated += random;
-        allocated_count++;
-        pointer[i] = memory_alloc(random);
-        if (pointer[i]) {
-            i++;
-            mallocated_count++;
-            mallocated += random;
-        }
-    }
-    for (int j = 0; j < i; j++) {
-        if (memory_check(pointer[j])) {
-            memory_free(pointer[j]);
-        }
-        else {
-            printf("Error: Wrong memory check.\n");
-        }
-    }
-    memset(region + 500, 0, random_memory);
-    for (int j = 0; j < 100000; j++) {
-        if (region[j] != 0) {
-            region[j] = 0;
-            printf("Error: Modified memory outside the managed region. index: %d\n",j-500);
-        }
-    }
-    float result = ((float)mallocated_count / (float)allocated_count) * 100;
-    float result_bytes = ((float)mallocated / (float)allocated) * 100;
-    printf("Memory size of %d bytes: allocated %.2f%% blocks (%.2f%% bytes).\n", random_memory, result, result_bytes);
-}
-
-
-//int main() {
-//    char region[100000];
-//    char* pointer[13000];
-//    z1_testovac(region, pointer, 8, 24, 50, 100, 1);
-//    z1_testovac(region, pointer, 8, 1000, 10000, 20000, 0);
-//    z1_testovac(region, pointer, 8, 35000, 50000, 60000, 0);
-//
-//    return 0;
-//}
-
 // ----------TESTY---------
 
 //int main(void){   //rovnake bloky do pola 50
@@ -678,37 +607,37 @@ void z1_testovac(char *region, char **pointer, int minBlock, int maxBlock, int m
 //    return 0;
 //}
 
-int main(void){     //ratanie chyby od idealneho riesenia
-    char region[5050];
-    memory_init(region, 5000);
-    char *pointers[5000];
-    int allocated = 0;
-    int allocated_count = 0;
-    int memSize = 5000;
-    int size;
-    int i = 0;
-    int freeSize;
-    do {
-        i++;
-        size = rand() % 200 + 8;
-        freeSize = *char2int(int2char(header) + *char2int(int2char(header) + 4));
-        if(freeSize < size){
-            size = 8;
-        }
-        allocated += size;
-        allocated_count++;
-        pointers[i] = memory_alloc(size);
-    } while(pointers[i] != NULL);
-    i = 1;
-    while(pointers[i] != NULL) {
-        memory_free(pointers[i]);
-        i++;
-    }
-    printf("velkost uspesne alokovanej pamate je %d v %d blokoch na pamat velkosti %d\n", allocated, allocated_count, memSize);
-    printf("To je %.2lf%%\n", (double) allocated / (double) memSize * 100);
-    if(*char2int(region + 8) == *char2int(region) - 16){
-        printf("Successful Test\n");
-    }
-
-    return 0;
-}
+//int main(void){     //ratanie chyby od idealneho riesenia
+//    char region[5050];
+//    memory_init(region, 5000);
+//    char *pointers[5000];
+//    int allocated = 0;
+//    int allocated_count = 0;
+//    int memSize = 5000;
+//    int size;
+//    int i = 0;
+//    int freeSize;
+//    do {
+//        i++;
+//        size = rand() % 200 + 8;
+//        freeSize = *char2int(int2char(header) + *char2int(int2char(header) + 4));
+//        if(freeSize < size){
+//            size = 8;
+//        }
+//        allocated += size;
+//        allocated_count++;
+//        pointers[i] = memory_alloc(size);
+//    } while(pointers[i] != NULL);
+//    i = 1;
+//    while(pointers[i] != NULL) {
+//        memory_free(pointers[i]);
+//        i++;
+//    }
+//    printf("velkost uspesne alokovanej pamate je %d v %d blokoch na pamat velkosti %d\n", allocated, allocated_count, memSize);
+//    printf("To je %.2lf%%\n", (double) allocated / (double) memSize * 100);
+//    if(*char2int(region + 8) == *char2int(region) - 16){
+//        printf("Successful Test\n");
+//    }
+//
+//    return 0;
+//}
